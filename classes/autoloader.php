3869e088689889523dbdc2fb751ec4d846ea98c4
Martijn <martijn@vanderlee.com>
2015-05-30 21:33:34 +0200

    Rename

--- classes/autoloader.php
+++ classes/autoloader.php
@@ -10,7 +10,6 @@
 	 * - http://www.ctan.org/tex-archive/language/hyph-utf8/tex/generic/hyph-utf8/patterns/tex/
 	 * - http://www.ctan.org/tex-archive/language/hyphenation/
 	 *
-	 * @version 0.2
 	 * @author Martijn W. van der Lee <martijn-at-vanderlee-dot-com>
 	 * @author Wim Muskee <wimmuskee-at-gmail-dot-com>
 	 * @copyright Copyright (c) 2011, Martijn W. van der Lee
