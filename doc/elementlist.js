8d3e931892cce8b86366a23df77f5687410fcd9b
Martijn <martijn@vanderlee.com>
2015-11-20 19:56:43 +0100

    Initial PHPDoc

---
+++ doc/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Syllable"],["f","Syllable_autoloader()"],["c","Syllable_Cache_FileAbstract"],["c","Syllable_Cache_Interface"],["c","Syllable_Cache_Json"],["c","Syllable_Cache_Serialized"],["c","Syllable_Hyphen_Dash"],["c","Syllable_Hyphen_Entity"],["c","Syllable_Hyphen_Interface"],["c","Syllable_Hyphen_Soft"],["c","Syllable_Hyphen_Text"],["c","Syllable_Hyphen_ZeroWidthSpace"],["c","Syllable_Source_File"],["c","Syllable_Source_Interface"]];
