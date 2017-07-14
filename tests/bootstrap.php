64a5c11ea9e7df32105ccd1b5830024fd97c7944
Martijn van der Lee <martijn@vanderlee.com>
2017-07-14 22:32:09 +0200

    Unittest modernizing

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once dirname(__DIR__) . '/classes/autoloader.php';
+
+// backward compatibility
+if (!class_exists('PHPUnit\Framework\TestCase') && class_exists('PHPUnit_Framework_TestCase')) {
+	class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+}
