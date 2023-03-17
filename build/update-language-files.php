b41f3bc3f16830219a4c5daa2a456eb70029e9b8
Alexander Nitsche <neos@alexandernitsche.com>
2023-03-17 23:17:26 +0100

    Move update script to common "build" folder
    - The cmd/ folder indicates a production context, so the script should
      be moved to build/ to make it clear that it is intended for package
      developers and not users.
    - The build/logs/ folder is already used by unit test runs. Apply
      .gitignore to this special folder instead of the entire build/.
    - Use automatic PSR-4 class loading from Composer.

---
+++ build/update-language-files.php
@@ -0,0 +1,7 @@
+<?php
+
+use Vanderlee\SyllableBuild\LanguageFileService;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+(new LanguageFileService())->updateLanguageFiles();
--- cmd/update-syllables.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once 'SyllableUpdate.php';
-
-(new SyllableUpdate())->updateLanguageFiles();
